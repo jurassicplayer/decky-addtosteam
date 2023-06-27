f0a69efbc1e25a900f98b087825a978600fdedb7
jurassicplayer <jurassicplayer75@gmail.com>
2023-06-27 11:21:36 -0700

    Add steamappdetails interface

---
+++ src/interface.ts
@@ -0,0 +1,9 @@
+export interface SteamAppDetails {
+  strDisplayName:string,
+  strLaunchOptions: string,
+  strManualURL:string,
+  strShortcutExe:string,
+  strShortcutLaunchOptions:string,
+  strShortcutStartDir:string,
+  unAppID:number,
+}
