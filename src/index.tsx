7cd66e7da4fde1c551ea5210231fda95e5f19437
jurassicplayer <jurassicplayer75@gmail.com>
2023-06-27 11:21:54 -0700

    Attempt handling failed shortcut generation

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,42 @@
 import {
   ButtonItem,
   definePlugin,
+  FileSelectionType,
   Navigation,
   PanelSection,
   PanelSectionRow,
   ServerAPI,
   staticClasses,
   ToastData,
+  
 } from "decky-frontend-lib";
 import { useEffect, useState, VFC } from "react";
 import { MdSwitchAccessShortcutAdd } from "react-icons/md";
+import { SteamAppDetails } from "./interface";
+
+async function getAppDetails(appID:number): Promise<SteamAppDetails|null> {
+  return new Promise((resolve) => {
+    let { unregister } = SteamClient.Apps.RegisterForAppDetails(appID, (details:SteamAppDetails) => {
+      unregister()
+      resolve(details.unAppID === undefined ? null : details)
+    })
+  })
+}
+
+async function addShortcut(appName: string, execPath: string): Promise<{failed: number|null, success: number|null}> {
+  return new Promise((resolve) => {
+    let failed:number|null = null
+    let success:number|null = null
+    SteamClient.Apps.AddShortcut(appName, execPath, "", "").then(async (appID:number) => {
+      let appDetails = await getAppDetails(appID)
+      if (appDetails && appDetails.strDisplayName && appDetails.strShortcutExe) {
+        success = appID
+      } else { failed = appID }
+      let results = {failed: failed, success: success}
+      resolve(results)
+    })
+  })
+}
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
   const [lastUsedPath, setLastUsedPath] = useState<string>('')
@@ -25,38 +52,61 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
   }
   const onClick = async () => {
     Navigation.CloseSideMenus()
-    serverAPI.openFilePicker(
+    let filepath = await serverAPI.openFilePickerV2(
+      FileSelectionType.FILE,
       lastUsedPath,
+      true,
+      undefined,
+      undefined,
+      undefined,
+      false,
       true
-    ).then((selection) => {
-      if (!selection.realpath) { return }
-      let file = selection.realpath.split(/[\\\/]/).pop()
-      let filename = file ? file.split('.').shift() : undefined
-      let path = selection.realpath.substring(0, selection.realpath.length - (file ? file.length : 0))
-      setLastUsedPath(path)
-      localStorage.setItem('decky-addtosteam', path)
-      if (!file && !filename) {
-        console.log(`Couldn't figure out filename for ${selection.realpath}`)
-        return
-      } 
-      let appName = filename
-      let execPath = selection.realpath
-      let toastData: ToastData = {
-        title: 'Added Shortcut',
-        body: file,
-        playSound: true,
-        showToast: true
-      }
-      SteamClient.Apps.AddShortcut(appName, execPath, "", "")
+    )
+    if (!filepath) { return }
+    // @ts-ignore
+    let file = filepath.realpath.split(/[\\\/]/).pop()
+    let filename = file
+    if (file) {
+      let newfilename = file.split('.')
+      newfilename.pop()
+      filename = newfilename.join('.')
+    }
+    let path = filepath.realpath.substring(0, filepath.realpath.length - (file ? file.length : 0))
+    setLastUsedPath(path)
+    localStorage.setItem('decky-addtosteam', path)
+    let appName = filename || file || ''
+    let execPath = filepath.realpath
+    let shortcutsFailed: number[] = []
+    let shortcutAdded: {failed: number|null, success: number|null} = {
+      failed: null,
+      success: null
+    }
+    let attempts = 0
+    while (!shortcutAdded.success && attempts < 5) {
+      shortcutAdded = await addShortcut(appName, execPath)
+      if (shortcutAdded.failed) { shortcutsFailed.push(shortcutAdded.failed) }
+      attempts += 1
+    }
+    shortcutsFailed.forEach((appID) => { SteamClient.Apps.RemoveShortcut(appID) })
+    console.log(`Add shortcut result (attempts ${attempts}): `, shortcutAdded.success)
+    let toastData: ToastData = {
+      title: '',
+      body: file,
+      playSound: true,
+      showToast: true
+    }
+    if (shortcutAdded.success) {
+      toastData.title = 'Added Shortcut'
       serverAPI.toaster.toast(toastData)
-    }).catch((err) => {
-      if (err == "User canceled") { return }
-      console.log(err)
-    })
+    } else {
+      toastData.title = 'Failed to add Shortcut'
+      toastData.sound = 4
+      serverAPI.toaster.toast(toastData)
+    }
   }
 
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection>
       <PanelSectionRow>
         <ButtonItem
           layout="below"
