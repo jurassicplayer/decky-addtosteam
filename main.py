da483587f5583ed53bcfea4d80c821f152d6d40e
jurassicplayer <jurassicplayer75@gmail.com>
2024-10-03 10:57:49 -0700

    Keep plugin overrides to help prevent decky-loader hanging

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import decky
 class Plugin:
     async def getDeckyUserHome(self):
         return decky.DECKY_USER_HOME
+    async def _main(self):
+        pass
+    async def _unload(self):
+        pass
+    async def _uninstall(self):
+        pass
+    async def _migration(self):
+        pass
